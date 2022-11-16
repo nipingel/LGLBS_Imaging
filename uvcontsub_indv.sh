5e9493a682e07dcfd001d4ab99f7c29efc7ade95
nipingel <nipingel@mix.wvu.edu>
2022-11-16 10:41:27 -0600

    add scripts for concatenating measurement sets (concat_all), re-weighting visibilities (statwt_*), and uv-based continuum subtraction (uvcontsub_*); also edit to unpack tarballs on compute nodes without copying file over and update tclean parameters for imaging

--- uvcontsub_indv.sh
+++ uvcontsub_indv.sh
@@ -3,27 +3,35 @@
 # uvcontsub_all.sh
 # execution script to shift phase centre of ASKAP measurement sets to user specified value
 
+## set up working directory
+mv uvcontsub_indv.py tmp
 cd tmp
-
-tar -xzf ../packages.tar.gz -directory .
-tar -xzf ../analysis_scripts.tar -directory .
+cp /scratch/casa-6.5.0-15-py3.8.tar.xz .
+xz -d casa-6.5.0-15-py3.8.tar.xz
+tar -xvf casa-6.5.0-15-py3.8.tar
+tar -xzf ../packages.tar.gz --directory .
+tar -xvf ../analysis_scripts.tar --directory .
 
 HOME=$PWD
 
-## untar measurement set to current working directory
-tar -xvf /projects/vla-processing/measurement_sets/WLM/$ms_name --directory .
 ## define untarred name
-tar_name=$1
+tar_name=$1".transformed.tar"
 untar_name=(${tar_name//.tar/ })
+
+## untar measurement set to current working directory
+tar -xvf /projects/vla-processing/measurement_sets/WLM/$tar_name --directory .
 
 v_sys="-125"
 v_width="210"
 order="0"
 
+## override system variables (**CHANGE THIS IN DOCKER FILE***)
+export USER=ipython
+export LOGNAME=ipython
 # make casa call to imaging script
-casa-6.5.0-15-py3.8/bin/casa --logfile uvcontsub.log -c uvcontsub_indv.py -p $untar_name -o $order -v $v_sys -w $v_width
+casa-6.5.0-15-py3.8/bin/casa --logfile uvcontsub.log -c uvcontsub_indv.py -n $untar_name -o $order -v $v_sys -w $v_width
 
 ## pack up and copy back
 tar -cvf $untar_name"_contsub.tar" $untar_name".contsub"
 mv $untar_name"_contsub.tar" /projects/vla-processing/measurement_sets/WLM
-rm $untar_name
+rm -rf $untar_name
