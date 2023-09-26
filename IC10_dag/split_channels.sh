588fa77e0a52ff6a8faa63071c44bb5342691528
Nickolas Pingel <nickolaspingel@dhcp-200.astro.wisc.edu>
2023-09-26 14:19:03 -0500

    edit submission scripts to ensure job lands on correct node

--- IC10_dag/split_channels.sh
+++ IC10_dag/split_channels.sh
@@ -9,7 +9,7 @@ HOME=$PWD
 ## assign variables
 ms_name=$1
 source_name=$2
-full_path="/projects/vla-processing/measurement_sets/"${source_name}"/"${ms_name}
+full_path=/projects/vla-processing/measurement_sets/${source_name}/${ms_name}
 
 # make casa call to imaging script
 /casa-6.5.0-15-py3.8/bin/casa --nologfile -c split_channels.py -p ${full_path} -s $3 -e $4 --indv_channel
