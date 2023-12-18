fb7ed150a362ca3621f8b5807a5a52bb1087d78a
Nickolas Pingel <nickolaspingel@dhcp-200.astro.wisc.edu>
2023-12-18 15:53:25 -0600

    run DAG up to imaging of individual channels; still need to split range of channels to split

---
+++ IC1613_dag/ms_transform.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# ms_trasform.sh
+# execution script to place all split-out measurement sets on same spectral axis & reference frame  
+
+## change home directory so CASA will run
+HOME=$PWD
+
+## assign inputs & script variables
+ms_name=$1
+chan_width=$2
+output_frame=$3
+src_name=$4
+untar_name=(${ms_name//.tar/ })
+output_ms_name=${untar_name}".transformed"
+
+## untar measurement set to current working directory
+tar -xvf /projects/vla-processing/measurement_sets/${src_name}/${ms_name} --directory .
+
+# make casa call to imaging script
+/casa-6.5.0-15-py3.8/bin/casa --nologfile -c ms_transform.py -p ${untar_name} -w ${chan_width} -o ${output_ms_name} -f ${output_frame}
+
+tar -cvf ${output_ms_name}".tar" ${output_ms_name}
+mv ${output_ms_name}".tar" /projects/vla-processing/measurement_sets/${src_name}
+
+## clean up
+rm -rf ${untar_name}
---
+++ IC1613_dag/split_channels.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# split_channels.sh
+# execution script to split out a range of channels from a staged and calibrated LGLBS measurement set for imaging
+
+## change home directory so CASA will run
+HOME=$PWD
+
+## assign variables
+ms_name=$1
+source_name=$2
+full_path=/projects/vla-processing/measurement_sets/${source_name}/${ms_name}
+
+# make casa call to imaging script
+/casa-6.5.0-15-py3.8/bin/casa --nologfile -c split_channels.py -p ${full_path} -s $3 -e $4 --indv_channel
+
+## tar measurement sets into files for each splitted-out channel
+cd /projects/vla-processing/measurement_sets/${source_name}
+
+## arithmetic for loop
+last_chan=$(($4-1))
+for i in $( eval echo {$3..$last_chan})
+do 
+	tar -cvf ${ms_name}"_chan"$i".tar" ${ms_name}"_chan"$i
+done
