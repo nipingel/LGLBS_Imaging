0c7d7ff3d1f890300d38633c44ed11179e6a704a
nipingel <nmpingel@wisc.edu>
2024-03-25 13:47:40 -0500

    image full cube, excluding problem track ...eb4002499...

--- IC1613_dag/image_track_per_scan.sh
+++ IC1613_dag/image_track_per_scan.sh
@@ -6,7 +6,6 @@
 ## change home directory so CASA will run
 HOME=$PWD
 
-$(chan) $(obs_id) $(scan_id) $(ms_name) $(src_name) $(ra_phase_center) $(dec_phase_center)
 ## assign variables
 chan_num=$1
 obs_id=$2
@@ -29,7 +28,7 @@ if [ "$1" -lt "100" ] && [ "$1" -gt "9" ]; then
 fi
 
 # make casa call to imaging script
-/casa-6.5.0-15-py3.8/bin/casa --logfile ${output_name}".log" -c image_channel_per_track.py -v ${ms_name} -o ${output_name} -i ${obs_id} -s ${scan_id}-r ${ra_phase_center} -d${dec_phase_center}
+/casa-6.5.0-15-py3.8/bin/casa --logfile ${output_name}".log" -c image_channel_per_track.py -v ${ms_name} -o ${output_name} -i ${obs_id} -s ${scan_id} -r ${ra_phase_center} -d${dec_phase_center}
 
 ## remove the original measurement set
 rm -rf ${ms_name}
