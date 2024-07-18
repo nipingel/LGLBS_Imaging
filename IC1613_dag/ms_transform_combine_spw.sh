8374f2793500eb49d6f69b8427bf2386d342d859
Nickolas Pingel <nickolaspingel@Nickolass-MacBook-Pro-2.local>
2024-07-17 22:26:03 -0500

    add concat to combine spw node

--- IC1613_dag/ms_transform_combine_spw.sh
+++ IC1613_dag/ms_transform_combine_spw.sh
@@ -9,7 +9,7 @@ HOME=$PWD
 ## assign inputs & script variables
 input_name=$1
 src_name=$2
-full_path=/projects/vla-processing/measurement_sets/${src_name}
+full_path=/projects/vla-processing/measurement_sets/${src_name}/single-field-imaging
 
 ## make call to casa
-/casa-6.5.0-15-py3.8/bin/casa --nologfile -c ms_transform_combine_spw.py -p ${full_path} -n ${input_name}
+casa --nologfile -c ms_transform_combine_spw.py -p ${full_path} -n ${input_name}
