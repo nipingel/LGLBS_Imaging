3cafbc7a0886eb19799352615d41fd934f015395
Nickolas Pingel <nickolaspingel@dhcp-250.astro.wisc.edu>
2024-07-23 13:08:51 -0500

    add feather scripts for WLM

---
+++ WLM_dag/feather_cubes.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# feather_cubes.sh
+# execution script to feather two cubes together using uvcombine
+
+## define variables
+src_name=$1
+sdcube=$2
+interfcube=$3
+outpath=$4
+galaxy=$5
+
+## set up python env
+source /miniconda3/etc/profile.d/conda.sh
+conda activate astro_env
+
+tar -xvf uvcombine.tar --directory /projects/vla-processing/images/${src_name}
+
+## change working directory to staging area
+mv feather_cubes.py /projects/vla-processing/images/${src_name}
+cd /projects/vla-processing/images/${src_name}
+
+echo $PATH
+python feather_cubes.py --sdcube ${sdcube} --interfcube ${interfcube} --outpath ${outpath} --galaxy ${galaxy}
