8464d170329dc07474eb838d3d892e4103928a28
nipingel <nmpingel@wisc.edu>
2025-04-02 11:38:32 -0500

    place python script and uvcombine package in staging directory to avoid overwriting for other processes

--- M33_dag/feather_cubes_distributed.sh
+++ M33_dag/feather_cubes_distributed.sh
@@ -16,10 +16,7 @@ end_chan=$7
 source /miniconda3/etc/profile.d/conda.sh
 conda activate astro_env
 
-tar -xvf uvcombine.tar --directory /projects/vla-processing/images/${src_name}
-
 ## change working directory to staging area
-mv feather_cubes_distributed.py /projects/vla-processing/images/${src_name}
 cd /projects/vla-processing/images/${src_name}
 
 echo $PATH
