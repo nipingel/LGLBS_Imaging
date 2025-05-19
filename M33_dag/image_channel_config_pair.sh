992b51462ae19307a0a0c507db34d4f7c9440f05
Nickolas Pingel <nickolaspingel@Nickolass-MacBook-Pro-2.local>
2025-05-19 17:34:33 -0500

    add capability to concat/statwt single configuations; use image_channel_config_pair scripts to clean a hardcoded single channel from config pairs (and single config)

--- M33_dag/image_channel_config_pair.sh
+++ M33_dag/image_channel_config_pair.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 #
-# image_channel_config_pair.sh
-# execution script for imaging a configuration pair in a single channel spectral channel
-
+# image_channel.sh
+# execution script for imaging single channel of LGLBS sources 
 
 ## change home directory so CASA will run
 HOME=$PWD
 
-$(ms_name) $(src_name) $(config_pair) $(ra_phase_center) $(dec_phase_center)
-
 ## assign variables
 ms_name=$1
 src_name=$2
-config_pair=$3
-ra_phase_center=$4
-dec_phase_center=$5
-
+ra_phase_center=$3
+dec_phase_center=$4
 ms_path=/projects/vla-processing/measurement_sets/${src_name}/config_pair_test/${ms_name}
-output_name=${ms_name}"_config_pair_"${config_pair}"_chan"${chan_num}
+output_name=${ms_name}"_dirty_chan830"
 
-# make mpicasa call to imaging script
-mpicasa -n 6 casa --logfile ${output_name}".log" -c image_channel.py - -o ${output_name} -r ${ra_phase_center} -d${dec_phase_center}
+# make casa call to imaging script
+mpicasa -n 6 casa --logfile ${output_name}".log" -c dirty_image_channel.py -v ${ms_path} -o ${output_name} -r ${ra_phase_center} -d${dec_phase_center}
 
 ## tar result
 tar -cvf ${output_name}".tar" ${output_name}*
-mv ${output_name}".tar" /projects/vla-processing/measurement_sets/${src_name}/config_pair_test/
+mv ${output_name}".tar" /projects/vla-processing/images/${src_name}/config_pair_test/${output_name}".tar"
