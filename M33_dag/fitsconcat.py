e71a8d109eeb901a8ca15448254dba10f88be58d
nipingel <nmpingel@wisc.edu>
2025-08-07 11:29:09 -0500

    use file-seeking to write dummy FITS file for collating images

--- M33_dag/fitsconcat.py
+++ M33_dag/fitsconcat.py
@@ -16,93 +16,102 @@ import os
 import glob
 import argparse
 import numpy as np
+from spectral_cube import SpectralCube
+from astropy.wcs import WCS
 from astropy.io import fits
 
 ## parse user arguments
 def parse_user_arguments():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-p', '--path', help = '<required> path to input FITS files', required = True)
-	parser.add_argument('-e', '--extension', help = '<required> is the suffix *-beam.fits or *-image-pb.fits; valid input is beam or image-pb', required = True)
-	parser.add_argument('-o', '--outname', help ='<required> output name of file (.fits automatically appended)', required = True)
-	args, unknown = parser.parse_known_args()
-	return args.path, args.outname, args.extension
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--path', help = '<required> path to input FITS files', required = True)
+    parser.add_argument('-e', '--extension', help = '<required> is the suffix *-beam.fits or *-image-pb.fits; valid input is beam or image-pb', required = True)
+    parser.add_argument('-o', '--outname', help ='<required> output name of file (.fits automatically appended)', required = True)
+    args, unknown = parser.parse_known_args()
+    return args.path, args.outname, args.extension
 
 ## make empty FITS cube
 def make_empty_image(imlist, path, outname):
-	"""
-	Generate an empty dummy FITS data cube. The FITS cube can exceed available
-	RAM.
+    """
+    Generate an empty dummy FITS data cube. The FITS cube can exceed available
+    RAM.
 
-	The 2D image dimensions are derived from the first image in the list.
-	The number of channels in the output cube is assumed to be the length
-	of the input list of images.
-	"""
+    The 2D image dimensions are derived from the first image in the list.
+    The number of channels in the output cube is assumed to be the length
+    of the input list of images.
+    """
 
-	with fits.open(imlist[0], memmap=True) as hud:
-		xdim, ydim = np.squeeze(hud[0].data).shape[-2:]
+    with fits.open(imlist[0], memmap=True) as hud:
+        xdim, ydim = np.squeeze(hud[0].data).shape[-2:]
 
-	zdim = int(len(imlist))
-	dims = tuple([zdim, ydim, xdim])
-	print("X-dimension: ", xdim)
-	print("Y-dimension: ", ydim)
-	print("Z-dimension: ", zdim)
-	print('\n')
+    zdim = int(len(imlist))
+    dims = tuple([zdim, ydim, xdim])
+    print("X-dimension: ", xdim)
+    print("Y-dimension: ", ydim)
+    print("Z-dimension: ", zdim)
+    print('\n')
+    
+    ## create dummy data - size is irrelevant, just number of dims
+    data = np.zeros((0, 0, 0), dtype=np.float32)
+    hdu = fits.PrimaryHDU(data=data)
+    header = hdu.header
+    ## make minimal header
+    while len(header) < (36 * 1 - 1):
+        header.append()  # Adds a blank card to the end
 
-	# create dummy header
-	dummy_data = np.zeros(dims, dtype=np.float32)
-	hdu = fits.PrimaryHDU(data=dummy_data)
-	header = hdu.header
+    ## update header with correct size
+    header["NAXIS1"] = dims[1]
+    header["NAXIS2"] = dims[2]
+    header["NAXIS3"] = dims[0]
+    header.tofile('%s/%s' % (path, outname))
 
-	## create memmapped file
-	filename = '%s/%s.dat' % (path, outname)
-	memmap_data = np.memmap(filename, dtype=np.float32, mode = 'w+', shape = dims)
-	## save to disk
-	memmap_data.flush()
-	#fits.writeto('%s/%s' % (path, outname), dummy_data, header = header, overwrite = True)
-
-	## make fits file
-	hdu.writeto(outname, overwrite=True)
-	del memmap_data
-	os.remove(filename)
-	return
+    with open("large.fits", "rb+") as fobj:
+        # Seek past the length of the header, plus the length of the
+        # Data we want to write.
+        # 4 is the number of bytes per value, i.e. abs(header['BITPIX'])/8
+        # (this example is assuming a 32-bit float)
+        file_length = len(header.tostring()) + (dims[0] * dims[1] * dims[2] * 4)
+        # FITS files must be a multiple of 2880 bytes long; the final -1
+        # is to account for the final byte that we are about to write.
+        file_length = ((file_length + 2880 - 1) // 2880) * 2880 - 1
+        fobj.seek(file_length)
+        fobj.write(b"\0") ## writes zeros
+    return
 
 ## fill cube with FITS images
 def fill_cube_with_images(imlist, path, outname):
-	"""
-	Fills the empty data cube with fits data.
+    """
+    Fills the empty data cube with fits data.
 
-	The number of channels in the output cube is assumed to be the length
-	of the input list of images.
-	"""
-	## open first image FITS to get header
-	first_im_hdu = fits.open(imlist[0])
+    The number of channels in the output cube is assumed to be the length
+    of the input list of images.
+    """
+    ## open first image FITS to get header
+    first_im_hdu = fits.open(imlist[0])
 
-	## open cube FITS
-	outhdu = fits.open('%s/%s' % (path, outname), memmap=True, mode="update")
-	outdata = outhdu[0].data
+    ## open cube FITS
+    outhdu = fits.open('%s/%s' % (path, outname), memmap=True, mode="update")
+    outdata = outhdu[0].data
 
-	max_chan =  int(len(imlist))
-	for ii in range(0, max_chan):
-		print(f"Processing channel {ii}/{max_chan}", end='\r')
-		with fits.open(imlist[ii], memmap=True) as hdu:
-			outdata[ii, :, :] = hdu[0].data
-	## update fits file
-	fits.writeto('%s/%s' % (path, outname), outdata, header = first_im_hdu[0].header, overwrite = True)
-	outhdu.close()
+    max_chan =  int(len(imlist))
+    for ii in range(0, max_chan):
+        print(f"Processing channel {ii}/{max_chan}", end='\r')
+        with fits.open(imlist[ii], memmap=True) as hdu:
+            outdata[ii, :, :] = hdu[0].data
+    ## update fits file
+    fits.writeto('%s/%s' % (path, outname), outdata, header = first_im_hdu[0].header, overwrite = True)
+    outhdu.close()
 
 def main():
-	## parse user arguments (path to FITS files)
-	fits_file_path, f_outname, f_ext = parse_user_arguments()
-	cube_outname = '%s.fits' % (f_outname)
-	print('%s/*.%s' % (fits_file_path, f_ext))
-	image_list = sorted(glob.glob('%s/*.%s' % (fits_file_path, f_ext)))
+    ## parse user arguments (path to FITS files)
+    fits_file_path, f_outname, f_ext = parse_user_arguments()
+    cube_outname = '%s.fits' % (f_outname)
+    print('%s/*.%s' % (fits_file_path, f_ext))
+    image_list = sorted(glob.glob('%s/*.%s' % (fits_file_path, f_ext)))
 
-	## make empty FITS cube
-    ## TODO: np.memmap to generate the zero'd array before writing to
-    ## fits file
-	make_empty_image(image_list, fits_file_path, cube_outname)
+    ## make empty FITS cube
+    # make_empty_image(image_list, fits_file_path, cube_outname)
 
-	## fill FITS cube
-	fill_cube_with_images(image_list, fits_file_path, cube_outname)
+    ## fill FITS cube
+    fill_cube_with_images(image_list, fits_file_path, cube_outname)
 if __name__ == '__main__':
-	main()
+    main()
