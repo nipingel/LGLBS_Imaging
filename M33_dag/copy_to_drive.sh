decc9c13c69de921f763bcfbb596c14f2a0dc203
nipingel <nmpingel@wisc.edu>
2025-08-22 09:39:34 -0500

    finalize script to use rclone to upload products to Google Drive

--- M33_dag/copy_to_drive.sh
+++ M33_dag/copy_to_drive.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-set -e
-
-usage () {
-  echo "usage: $(basename "$0") remotepath dest"
-  exit 1
-} >&2
-
-[[ $1 && -d $2 ]] || usage
-
 src=$1
 dest=$2
 
@@ -17,8 +8,9 @@ staging_dir=/projects/vla-processing/images/M33
 rclone_base=/projects/vla-processing/software/rclone
 
 PATH=$PATH:${rclone_base}
-conf=${rclone_base}/config/rclone.conf
+conf=${rclone_base}/rclone.conf
 
+echo "here"
 rclone copy "${staging_dir}/${src}" "${dest}" --config "${conf}"
 
-touch "${dest_file}.done"
+touch "${src}.done"
